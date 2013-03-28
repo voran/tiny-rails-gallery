4cd8eec2232a3c8ee29679e485a92191bf9ad1e7
Yavor Stoychev <stoychev.yavor@gmail.com>
2013-03-28 03:03:57 +0200

    fixed home link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Gallery::Application.routes.draw do
   match "/browse(.:format)/(/*dir)" => "application#browse"
-  root :to => redirect("/browse/.html/")
+  root :to => redirect("/browse.html/")
 end
