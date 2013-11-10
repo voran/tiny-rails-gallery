dd1f975cd0dc7c3efc55da6136ca237a6399d054
Yavor Stoychev <yavor.stoychev@transmetrics.eu>
2013-11-09 16:37:52 -0800

    extract file and dir matching in seperate method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def browse
+    @entry =  params[:dir] || ""
+    @output = find_entries(Rails.public_path + Rails.configuration.thumbnails_url + @entry)
+    
+    respond_to do |format|
+      format.html { render }
+      format.json { render :json => @output}
+      format.xml {render :xml => @output.to_xml(:root => 'output')}
+    end
+  end
+  
+  def find_entries(entry_path)
     files = Array.new
     subdirs = Array.new
-    
-    @entry =  params[:dir] || ""
-    
-    entry_path = Rails.public_path + Rails.configuration.thumbnails_url + @entry
     Dir.foreach(entry_path) do |subentry|
       subentry_path = entry_path + '/' + subentry
       if File.directory?(subentry_path) and !Rails.configuration.ignore_dirs.include? subentry
@@ -16,13 +23,6 @@ class ApplicationController < ActionController::Base
         files.push(subentry)
       end
     end
-
-    @output = { :subdirs => subdirs.sort.reverse, :files => files.sort }
-    
-    respond_to do |format|
-      format.html { render }
-      format.json { render :json => @output}
-      format.xml {render :xml => @output.to_xml(:root => 'output')}
-    end
+    return { :subdirs => subdirs.sort.reverse, :files => files.sort }
   end
 end
